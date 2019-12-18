ecdc27862f83df9f2bafb218936f4006fd3d6326
zbr014 <765451419@qq.com>
2019-12-18 13:28:23 +0800

    添加关联表

--- connect1.js
+++ connect1.js
@@ -97,23 +97,23 @@ async function query() {
   const cursor = studentForm.find({}).cursor();
   let doc;
   while ((doc = await cursor.next())) {
-    const form = await ssForm.findOne({ 个人编号: doc.个人编号 });
+    const form = await ssForm.findOne({ 公民身份号码: doc.身份证号 });
     if (form) {
       const person = {
-        考生号: String,
-        考生姓名: String,
-        身份证号: String,
-        性别代码: String,
-        出生日期: String,
-        毕业类别代码: String,
-        户籍地代码: String,
-        录取院校代码: String,
-        录取院校名称: String,
-        录取专业名称: String,
-        录取层次代码: String,
-        录取标准代码: String,
-        录取批次代码: String,
-        录取日期: String,
+        考生号: doc.考生号,
+        考生姓名: doc.考生姓名,
+        身份证号: doc.身份证号,
+        性别代码: doc.性别代码,
+        出生日期: doc.出生日期,
+        毕业类别代码: doc.毕业类别代码,
+        户籍地代码: doc.户籍地代码,
+        录取院校代码: doc.录取院校代码,
+        录取院校名称: doc.录取院校名称,
+        录取专业名称: doc.录取专业名称,
+        录取层次代码: doc.录取层次代码,
+        录取标准代码: doc.录取标准代码,
+        录取批次代码: doc.录取批次代码,
+        录取日期: doc.录取日期,
         证书情况: form.证书情况,
         转移历史: form.转移历史,
         单位编号: form.单位编号,
@@ -123,14 +123,18 @@ async function query() {
         退出年份: form.退出年份,
         转出地域: form.转出地域,
         证书等级: form.证书等级,
-        '国家职业资格等级（工人技术等级）]':form['国家职业资格等级（工人技术等级）]'],
+        '国家职业资格等级（工人技术等级）]':
+          form['国家职业资格等级（工人技术等级）]'],
         专业技术职务: form.专业技术职务,
         '行政职务(级别)': form['行政职务(级别)'],
         社保所在地区: form.社保所在地区
       };
       const psn = new connectForm(person);
       await psn.save();
-    } else continue;
+    } else {
+      console.log(`${count++}`);
+      continue;
+    }
     console.log(`${count++}`);
   }
 
