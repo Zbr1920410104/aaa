82c014feef84be847962c8ecb65e03b4a717f5af
zbr014 <765451419@qq.com>
2019-12-18 09:39:38 +0800

    创建选择文件

--- create.js
+++ create.js
@@ -22,10 +22,15 @@ async function query() {
     录取专业名称: String,
     录取层次代码: String,
     录取标准代码: String,
-    录取批次代码: String
+    录取批次代码: String,
+    录取日期: String
   });
 
-  let studentModel = mongoose.model('Student', studentSchema, 'Student');
+  let studentModel = mongoose.model(
+    '关联-高考库报名录取信息表',
+    studentSchema,
+    '关联-高考库报名录取信息表'
+  );
 
   let registerSchema = mongoose.Schema({
     考生号: String,
@@ -51,7 +56,8 @@ async function query() {
     录取专业名称: String,
     录取层次代码: String,
     录取标准代码: String,
-    录取批次代码: String
+    录取批次代码: String,
+    录取日期: String
   });
 
   let enrollForm = mongoose.model(
@@ -65,23 +71,26 @@ async function query() {
   let doc;
   while ((doc = await cursor.next())) {
     const form = await registerForm.findOne({ 考生号: doc.考生号 });
-    const student = {
-      考生号: form.考生号,
-      考生姓名: form.考生姓名,
-      身份证号: form.报名所持有效证件号码,
-      性别代码: form.性别代码,
-      出生日期: form.出生日期,
-      毕业类别代码: form.毕业类别代码,
-      户籍地代码: form.户籍地代码,
-      录取院校代码: doc.录取院校代码,
-      录取院校名称: doc.录取院校名称,
-      录取专业名称: doc.录取专业名称,
-      录取层次代码: doc.录取层次代码,
-      录取标准代码: doc.录取标准代码,
-      录取批次代码: doc.录取批次代码
-    };
-    const stu = new studentModel(student);
-    await stu.save();
+    if (form) {
+      const student = {
+        考生号: form.考生号,
+        考生姓名: form.考生姓名,
+        身份证号: form.报名所持有效证件号码,
+        性别代码: form.性别代码,
+        出生日期: form.出生日期,
+        毕业类别代码: form.毕业类别代码,
+        户籍地代码: form.户籍地代码,
+        录取院校代码: doc.录取院校代码,
+        录取院校名称: doc.录取院校名称,
+        录取专业名称: doc.录取专业名称,
+        录取层次代码: doc.录取层次代码,
+        录取标准代码: doc.录取标准代码,
+        录取批次代码: doc.录取批次代码,
+        录取日期: doc.录取日期
+      };
+      const stu = new studentModel(student);
+      await stu.save();
+    } else continue;
     console.log(`${count++}`);
   }
 
